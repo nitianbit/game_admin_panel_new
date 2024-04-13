bd2703d20be3890686033b319e8c23d5b5b3da3e
Tushar Mangla <tushar@logicfirst.io>
2024-04-13 12:02:10 +0530

    api handler changes

---
+++ src/services/api/constants.js
@@ -0,0 +1,13 @@
+
+export const API_METHODS = {
+    GET: 'GET',
+    POST: 'POST',
+    DELETE: 'DELTE',
+    PUT: 'PUT'
+}
+
+export const ENDPOINTS = {
+    login: '/auth/login',
+    register: '/auth/signup',
+    profile: '/auth/profile',
+}
