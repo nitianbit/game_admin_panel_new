d74cd416276b6b5daa34a3595b82dce2828095fc
Tushar <tushar.mangla1120@gmail.com>
2024-04-20 03:25:29 +0530

    user changes

---
+++ src/pages/Users/Users.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { Card, CardBody, CardFooter, CardHeader } from 'react-bootstrap';
+import { Table } from '../../components'
+import { ENDPOINTS } from './UsersConstant';
+import { doGET } from '../../utils/HttpUtil';
+
+const Users =()=> {
+
+  const [users, setUsers] = useState()
+    
+  const getAllUsers = async (e) => {
+      try {
+          const response = await doGET(ENDPOINTS.allUsers);
+          setUsers(response?.data)
+      } catch (error) { }
+  };
+
+  useEffect(()=>{
+      getAllUsers()
+  },[])
+    return (
+        <div className='d-flex w-100'>
+
+        <Table tableData={users}/>
+        </div>
+    )
+}
+
+
+export default Users
