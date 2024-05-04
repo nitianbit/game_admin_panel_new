59b49089bbd3091cd5ae87ceae4209d96974caed
Tushar Mangla <tushar.mangla1120@gmail.com>
2024-05-05 02:13:10 +0530

    basic info dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -3,16 +3,17 @@ import { Card, CardBody, CardFooter, CardHeader } from 'react-bootstrap';
 import './style.css'
 import { doGET } from '../../utils/HttpUtil';
 import CurrentContest from './CurrentContest';
+import DashboardBasicInfo from './DashboardBasicInfo'
 function Dashboard() {
 
 
     return (
         <div className='d-flex w-100'>
-
-            {/* <Table/> */}
-            <div className='w-50'>
-
+            <div className='w-50 me-3'>
                 <CurrentContest />
+            </div>
+            <div>
+                <DashboardBasicInfo />
             </div>
         </div>
     )
